3dbea0b3d14ebf020a75cbed08c0d960e3255efd
Pankaj Goenka <psgoenka@gmail.com>
2022-11-28 18:02:30 +0530

    loop commit

--- src/test/loop_demo.java
+++ src/test/loop_demo.java
@@ -3,6 +3,27 @@ package test;
 public class loop_demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        while loop
+        int i=10;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+//        do while loop
+        int j=20;
+        do{
+            System.out.println(j);
+            j++;
+        }while(j>30);
+
+//        for loop
+        for(int x=0; x<10; x=x+3){
+            if(x==9){
+                System.out.println("X value is equal to 9");
+            } else {
+                System.out.println("I didn't find value");
+            }
+        }
     }
 }
